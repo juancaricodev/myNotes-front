982ea2a3593a70da690a1d4652426e1f489e9c13
Juan Camilo Rico <juancarico92@gmail.com>
2021-04-20 11:04:20 -0500

    feat(Layout): component created

---
+++ src/layouts/Layout/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import Header from './components/Header'
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Header />
+      <div className='layout'>
+        {children}
+      </div>
+    </>
+  )
+}
+
+export default Layout
